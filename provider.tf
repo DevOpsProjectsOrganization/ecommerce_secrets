c95a064b836259e58968a489489007585589c5c1
Sridevi <parimisridevi586@gmail.com>
2025-07-19 16:11:31 +1000

    added the mount and the secret values

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
 # add the s3 bucket tool
 terraform {
     backend "s3" {
-        name = "terraform-bucket-85"
+        bucket = "terraform-bucket-85"
         key  = "/vault/terraform.tfstate"
         region = "us-east-1"
     }
