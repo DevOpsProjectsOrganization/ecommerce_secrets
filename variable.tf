311c6eca0100cdc14fdca3e83098f79b7a140453
Sridevi <parimisridevi586@gmail.com>
2025-07-30 20:57:28 +1000

    fixing errors

--- variable.tf
+++ variable.tf
@@ -34,11 +34,11 @@ variable "secrets" {
         frontend = {
             secret_mount    = "roboshop-dev"
             kv              = {
-                CATALOGUE_URL   = "http://catalogue-dev.sdevops.shop:8080",
-                USER_URL        = "http://user-dev.sdevops.shop:8080",
-                CART_URL        = "http://cart-dev.sdevops.shop:8080",
-                SHIPPING_URL    = "http://shipping-dev.sdevops.shop:8080",
-                PAYMENT_URL     = "http://payment-dev.sdevops.shop:8080"
+                CATALOGUE_URL   = "http://catalogue-dev.sdevops.shop:8080/",
+                USER_URL        = "http://user-dev.sdevops.shop:8080/",
+                CART_URL        = "http://cart-dev.sdevops.shop:8080/",
+                SHIPPING_URL    = "http://shipping-dev.sdevops.shop:8080/",
+                PAYMENT_URL     = "http://payment-dev.sdevops.shop:8080/"
             }
         }
         payment = {
